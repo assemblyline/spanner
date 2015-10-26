f10713a5b16e99aa9b1133f69b0e6e599982f363
Ed Robinson <ed.robinson@reevoo.com>
2015-10-26 23:20:24 +0000

    Testable Logger

--- logger/logger.go
+++ logger/logger.go
@@ -3,18 +3,32 @@ package logger
 import (
 	"fmt"
 	"github.com/mgutz/ansi"
+	"io"
+	"os"
 	"strings"
 )
 
 var title = ansi.ColorFunc("black+b:yellow")
 var info = ansi.ColorFunc("blue")
 
-func LogTitle(text ...string) {
-	text = append([]string{"["}, text...)
-	text = append(text, "]")
-	fmt.Println(title(strings.Join(text, " ")))
+type Logger struct {
+	out io.Writer
+	err io.Writer
 }
 
-func LogInfo(text ...string) {
-  fmt.Println(info(strings.Join(text, " ")))
+func New() Logger {
+	return Logger{
+		out: os.Stdout,
+		err: os.Stderr,
+	}
+}
+
+func (l Logger) Title(text ...string) {
+	text = append([]string{"["}, text...)
+	text = append(text, "]")
+	fmt.Fprintln(l.out, title(strings.Join(text, " ")))
+}
+
+func (l Logger) Info(text ...string) {
+	fmt.Fprintln(l.out, info(strings.Join(text, " ")))
 }
---
+++ logger/logger_test.go
@@ -0,0 +1,35 @@
+package logger
+
+import (
+	"bytes"
+	"github.com/mgutz/ansi"
+	. "github.com/smartystreets/goconvey/convey"
+	"os"
+	"testing"
+)
+
+func TestCache(t *testing.T) {
+	Convey("logging", t, func() {
+		out := &bytes.Buffer{}
+		log := Logger{
+			out: out,
+		}
+
+		Convey("New sets up logger with standard out and err", func() {
+			newLogger := New()
+
+			So(newLogger.out, ShouldEqual, os.Stdout)
+			So(newLogger.err, ShouldEqual, os.Stderr)
+		})
+
+		Convey("titles", func() {
+			log.Title("foo", "bar", "baz")
+			So(out.String(), ShouldContainSubstring, ansi.Color("[ foo bar baz ]", "black+b:yellow"))
+		})
+
+		Convey("info", func() {
+			log.Info("foo", "bar", "baz")
+			So(out.String(), ShouldContainSubstring, ansi.Color("foo bar baz", "blue"))
+		})
+	})
+}
